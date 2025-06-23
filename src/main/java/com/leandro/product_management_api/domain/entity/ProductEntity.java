8f0ffc317ef6b5e3eec2c31a72986753b72df22b
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-23 17:20:04 -0300

    create entity

---
+++ src/main/java/com/leandro/product_management_api/domain/entity/ProductEntity.java
@@ -0,0 +1,41 @@
+package com.leandro.product_management_api.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Product")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+
+    public ProductEntity(String name, BigDecimal price, Integer stock){
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public void updateProduct(String name, BigDecimal price, Integer stock){
+        if(name != null && !name.isBlank()){this.name = name;}
+        if(price != null && price.compareTo(BigDecimal.ZERO) >= 0){this.price = price;}
+        if(stock != null && stock >= 0){this.stock = stock;}
+    }
+}
