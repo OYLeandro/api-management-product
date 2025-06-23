7c5fc738bb1788144d680b5ab15b2ad4c5a9aaa4
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-23 19:10:26 -0300

    rename method

---
+++ src/main/java/com/leandro/product_management_api/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.leandro.product_management_api.mapper;
+
+import com.leandro.product_management_api.domain.entity.ProductEntity;
+import com.leandro.product_management_api.dtos.requestdtos.ProductRequestDTO;
+import com.leandro.product_management_api.dtos.responsedtos.ProductResponseDTO;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    ProductResponseDTO toDto(ProductEntity entity);
+
+    ProductEntity toEntity(ProductRequestDTO dto);
+
+    List<ProductResponseDTO> toDtoList (List<ProductEntity> entity);
+}
