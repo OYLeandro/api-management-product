7e6cb43b8767c517e5c86925e7fcba688b0ec549
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-23 18:35:55 -0300

    add methed construct

--- src/main/java/com/leandro/product_management_api/domain/dtos/responsedtos/ProductResponseDTO.java
+++ src/main/java/com/leandro/product_management_api/domain/dtos/responsedtos/ProductResponseDTO.java
@@ -1,11 +1,11 @@
 package com.leandro.product_management_api.domain.dtos.responsedtos;
 
+import com.leandro.product_management_api.domain.entity.ProductEntity;
+
 import java.math.BigDecimal;
 
-public record ProductResponseDTO(
-        Long id,
-        String name,
-        BigDecimal price,
-        Integer stock
-) {
+public record ProductResponseDTO(Long id, String name, BigDecimal price, Integer stock) {
+    public ProductResponseDTO(ProductEntity entity){
+        this(entity.getId(), entity.getName(), entity.getPrice(), entity.getStock());
+    }
 }
