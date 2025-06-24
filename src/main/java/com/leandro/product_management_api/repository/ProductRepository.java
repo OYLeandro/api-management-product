122fba1539d78ed7c940548c12c36cfb1675cc9e
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-24 01:01:25 -0300

    correct type method

--- src/main/java/com/leandro/product_management_api/repository/ProductRepository.java
+++ src/main/java/com/leandro/product_management_api/repository/ProductRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
 
-    Page<ProductEntity> findByCategory(String category, Pageable pageable);
+   Optional<Page<ProductEntity>> findByCategory(String category, Pageable pageable);
 }
