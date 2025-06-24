79432495296c731d579f5b979473a5ba92cf5261
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-24 01:50:25 -0300

    create dto for registerProduct

--- src/main/java/com/leandro/product_management_api/mapper/ProductMapper.java
+++ src/main/java/com/leandro/product_management_api/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.leandro.product_management_api.mapper;
 
 import com.leandro.product_management_api.domain.entity.ProductEntity;
+import com.leandro.product_management_api.dtos.createdtos.ProductCreateDTO;
 import com.leandro.product_management_api.dtos.requestdtos.ProductRequestDTO;
 import com.leandro.product_management_api.dtos.responsedtos.ProductResponseDTO;
 import org.mapstruct.Mapper;
@@ -9,6 +10,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    ProductEntity toEntity(ProductCreateDTO dto);
+
     ProductResponseDTO toDto(ProductEntity entity);
 
     ProductEntity toEntity(ProductRequestDTO dto);
