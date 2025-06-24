2709abc12a933f4effe0f86c39e35bcae6f9ff78
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-23 23:08:43 -0300

    switching to simple model

--- src/main/java/com/leandro/product_management_api/domain/entity/ProductEntity.java
+++ src/main/java/com/leandro/product_management_api/domain/entity/ProductEntity.java
@@ -2,15 +2,15 @@ package com.leandro.product_management_api.domain.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "Products")
 @Getter
+@Setter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductEntity {
     @Id
@@ -26,16 +26,6 @@ public class ProductEntity {
     @Column(nullable = false)
     private Integer stock;
 
-
-    public ProductEntity(String name, BigDecimal price, Integer stock){
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-    }
-
-    public void updateProduct(String name, BigDecimal price, Integer stock){
-        if(name != null && !name.isBlank()){this.name = name;}
-        if(price != null && price.compareTo(BigDecimal.ZERO) >= 0){this.price = price;}
-        if(stock != null && stock >= 0){this.stock = stock;}
-    }
+    @Column(nullable = false)
+    private String category;
 }
