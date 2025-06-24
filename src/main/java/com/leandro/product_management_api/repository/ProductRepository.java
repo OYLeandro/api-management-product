f2c03fe5d096d4a0e75a9ead148325c974c7f9b0
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-23 23:50:49 -0300

    adding list method with page

--- src/main/java/com/leandro/product_management_api/repository/ProductRepository.java
+++ src/main/java/com/leandro/product_management_api/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.leandro.product_management_api.repository;
 
 import com.leandro.product_management_api.domain.entity.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
+
+    Page<ProductEntity> findByCategory(String category, Pageable pageable);
 }
