a513b61fbcfa8074943d20ddea928f93c73b3f9d
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-24 17:23:17 -0300

    creating the SecurityConfig package and disabling the default web

---
+++ src/main/java/com/leandro/product_management_api/configuration/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.leandro.product_management_api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception{
+        return security.csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
