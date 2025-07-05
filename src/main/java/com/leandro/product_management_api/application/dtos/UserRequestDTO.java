ac77fdc1f9b0f3f27e8d8f19db98d79603c67992
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-05 19:50:04 -0300

    correcting parameter

--- src/main/java/com/leandro/product_management_api/application/dtos/UserRequestDTO.java
+++ src/main/java/com/leandro/product_management_api/application/dtos/UserRequestDTO.java
@@ -1,8 +1,10 @@
 package com.leandro.product_management_api.application.dtos;
 
+import com.leandro.product_management_api.core.domain.role.UserRole;
+
 public record UserRequestDTO(
         String email,
         String password,
-        String role
+        UserRole role
 ) {
 }
