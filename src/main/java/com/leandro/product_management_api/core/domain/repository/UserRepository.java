d56a15bb1518e2fa4b8e72275b0cb9ae46251d32
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-05 20:58:35 -0300

    create new method

--- src/main/java/com/leandro/product_management_api/core/domain/repository/UserRepository.java
+++ src/main/java/com/leandro/product_management_api/core/domain/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 public interface UserRepository {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    boolean existsById(Long Id);
     User save(User user);
     void delete(User user);
     void deleteById(Long id);
