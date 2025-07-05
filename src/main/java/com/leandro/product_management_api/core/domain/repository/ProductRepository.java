b3a5b323ca65047b037e12ae4bd78e5695ada08e
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-05 14:54:37 -0300

    ignore this commit

--- src/main/java/com/leandro/product_management_api/core/domain/repository/ProductRepository.java
+++ src/main/java/com/leandro/product_management_api/core/domain/repository/ProductRepository.java
@@ -1,6 +1,10 @@
 package com.leandro.product_management_api.core.domain.repository;
 
 
+import com.leandro.product_management_api.core.domain.pagination.PageRequest;
+import com.leandro.product_management_api.core.domain.entity.Product;
+import com.leandro.product_management_api.core.domain.pagination.Page;
+
 public interface ProductRepository {
     Page<Product> findAll(PageRequest request);
 }
