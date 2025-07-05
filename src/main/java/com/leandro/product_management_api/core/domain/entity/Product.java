9888b4c798390fb8d694dfee0a74f2fa2f9d918f
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-05 13:57:01 -0300

    adding validations

--- src/main/java/com/leandro/product_management_api/core/domain/entity/Product.java
+++ src/main/java/com/leandro/product_management_api/core/domain/entity/Product.java
@@ -1,5 +1,10 @@
 package com.leandro.product_management_api.core.domain.entity;
 
+import com.leandro.product_management_api.core.domain.exception.CategoryProductInvalidException;
+import com.leandro.product_management_api.core.domain.exception.NameProductInvalidException;
+import com.leandro.product_management_api.core.domain.exception.PriceProductInvalidException;
+import com.leandro.product_management_api.core.domain.exception.StockProductInvalidException;
+
 import java.math.BigDecimal;
 
 public class Product {
@@ -10,7 +15,10 @@ public class Product {
     private String category;
 
     public Product(String name, BigDecimal price, Integer stock, String category){
-
+        if(name == null || name.isBlank()){throw new NameProductInvalidException();}
+        if(price == null || price.compareTo(BigDecimal.ZERO) < 0){throw new PriceProductInvalidException();}
+        if(stock == null || stock < 0){throw new StockProductInvalidException();}
+        if(category == null || category.isBlank()){throw new CategoryProductInvalidException();}
         this.name = name;
         this.price = price;
         this.stock = stock;
