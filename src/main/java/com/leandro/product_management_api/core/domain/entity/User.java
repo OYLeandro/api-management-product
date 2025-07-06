240c072a588ca80cb4b1d660577d72b4c8fe0f71
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-05 21:29:51 -0300

    correcting the class

--- src/main/java/com/leandro/product_management_api/core/domain/entity/User.java
+++ src/main/java/com/leandro/product_management_api/core/domain/entity/User.java
@@ -1,8 +1,5 @@
 package com.leandro.product_management_api.core.domain.entity;
 
-import com.leandro.product_management_api.core.domain.exception.EmailInvalidException;
-import com.leandro.product_management_api.core.domain.exception.PasswordInvalidException;
-import com.leandro.product_management_api.core.domain.exception.RoleInvalidException;
 import com.leandro.product_management_api.core.domain.role.UserRole;
 
 public class User {
@@ -12,26 +9,20 @@ public class User {
     private UserRole role;
 
     public User(String email, String password, UserRole role){
-        if(email == null || email.isBlank()){ throw new EmailInvalidException(email);}
-        if(password == null || password.isBlank()){throw new PasswordInvalidException(password);}
-        if(role == null){throw new RoleInvalidException();}
-        this.email = email.trim();
-        this.password = password.trim();
+        this.email = email;
+        this.password = password;
         this.role = role;
     }
 
     public void updateEmail(String email){
-        if (email == null || email.isBlank()){throw new EmailInvalidException("Email invalid "+ email);}
-        this.email = email.trim();
+        this.email = email;
     }
 
     public void updatePassword(String password){
-        if (password == null || password.isBlank()){throw new PasswordInvalidException("Password invalid "+ password);}
-        this.password = password.trim();
+        this.password = password;
     }
 
     public void updateRole(UserRole role){
-        if (role == null){throw new RoleInvalidException();}
         this.role = role;
     }
 
