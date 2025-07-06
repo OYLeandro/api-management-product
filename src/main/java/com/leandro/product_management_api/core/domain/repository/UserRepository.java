d935b6a9e5028f6ca66ad88235cae7b9912a90a1
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-05 22:12:54 -0300

    correcting

--- src/main/java/com/leandro/product_management_api/core/domain/repository/UserRepository.java
+++ src/main/java/com/leandro/product_management_api/core/domain/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public interface UserRepository {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    boolean existsById(Long Id);
+    boolean existsById(Long id);
     User save(User user);
     void delete(User user);
     void deleteById(Long id);
