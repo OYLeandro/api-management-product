752a1824f78fd929f9b066bfa0c1cf6a10a5867f
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-06 00:45:42 -0300

    created new impl

---
+++ src/main/java/com/leandro/product_management_api/infra/persistence/PasswordHashedImpl.java
@@ -0,0 +1,20 @@
+package com.leandro.product_management_api.infra.persistence;
+
+import com.leandro.product_management_api.application.interfaces.PasswordHasher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordHashedImpl implements PasswordHasher {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public boolean matches(String password, String hashedPassword) {
+        return passwordEncoder.matches(password, hashedPassword);
+    }
+}
