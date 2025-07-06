be733d75a2d6ee94eb0115014904613ca6a37bf6
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-06 00:23:39 -0300

    correcting

--- src/main/java/com/leandro/product_management_api/infra/service/TokenServiceImpl.java
+++ src/main/java/com/leandro/product_management_api/infra/service/TokenServiceImpl.java
@@ -4,7 +4,10 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.leandro.product_management_api.application.security.TokenProvider;
+import com.leandro.product_management_api.core.domain.entity.User;
 import com.leandro.product_management_api.infra.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +16,18 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 @Service
-public class TokenService {
-    @Value("${api.security.token.secret}")
-    private String secret;
-
+public class TokenServiceImpl implements TokenProvider {
+    private final String secret;
+    private final Algorithm algorithm;
     private static final String issuer = "Product-management-api";
 
-    public String generateToken(UserEntity user){
+    public TokenServiceImpl(@Value("${api.security.token.secret}") String secret){
+        this.secret = secret;
+        this.algorithm = Algorithm.HMAC256(secret);
+    }
+
+    @Override
+    public String generateToken(User user){
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return  JWT.create()
@@ -32,7 +40,22 @@ public class TokenService {
         }
     }
 
-    public String validateToken(String token){
+    @Override
+    public boolean validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWT.require(algorithm)
+                    .withIssuer(issuer)
+                    .build()
+                    .verify(token);
+            return true;
+        }catch (JWTVerificationException e){
+            return false;
+        }
+    }
+
+    @Override
+    public String getUsernameFromToken(String token) {
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
@@ -46,6 +69,6 @@ public class TokenService {
     }
 
     private Instant generateExpiration(){
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.UTC);
     }
 }
