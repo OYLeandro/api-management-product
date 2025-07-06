013edc9f1367b4664c9b4362836f8b7acccdce61
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-06 00:03:12 -0300

    create new interfacer

---
+++ src/main/java/com/leandro/product_management_api/application/security/TokenProvider.java
@@ -0,0 +1,9 @@
+package com.leandro.product_management_api.application.security;
+
+import com.leandro.product_management_api.core.domain.entity.User;
+
+public interface TokenProvider {
+    String generateToken(User user);
+    boolean validateToken(String token);
+    String getUsernameFromToken(String token);
+}
