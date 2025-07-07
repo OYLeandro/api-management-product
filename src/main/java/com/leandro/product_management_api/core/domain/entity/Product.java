7ab46d36f97ffecb6d7ddedee391b1b6680dfc10
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-07 18:54:05 -0300

    adding the methods

--- src/main/java/com/leandro/product_management_api/core/domain/entity/Product.java
+++ src/main/java/com/leandro/product_management_api/core/domain/entity/Product.java
@@ -1,10 +1,5 @@
 package com.leandro.product_management_api.core.domain.entity;
 
-import com.leandro.product_management_api.core.domain.exception.CategoryProductInvalidException;
-import com.leandro.product_management_api.core.domain.exception.NameProductInvalidException;
-import com.leandro.product_management_api.core.domain.exception.PriceProductInvalidException;
-import com.leandro.product_management_api.core.domain.exception.StockProductInvalidException;
-
 import java.math.BigDecimal;
 
 public class Product {
@@ -15,13 +10,44 @@ public class Product {
     private String category;
 
     public Product(String name, BigDecimal price, Integer stock, String category){
-        if(name == null || name.isBlank()){throw new NameProductInvalidException();}
-        if(price == null || price.compareTo(BigDecimal.ZERO) < 0){throw new PriceProductInvalidException();}
-        if(stock == null || stock < 0){throw new StockProductInvalidException();}
-        if(category == null || category.isBlank()){throw new CategoryProductInvalidException();}
         this.name = name;
         this.price = price;
         this.stock = stock;
         this.category = category;
+    }
+
+    public void updateName (String name){
+        this.name = name;
+    }
+
+    public void updatePrice(BigDecimal price){
+        this.price = price;
+    }
+
+    public void updateStock(Integer stock){
+        this.stock = stock;
+    }
+
+    public void updateCategory(String category){
+        this.category = category;    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public String getCategory() {
+        return category;
     }
 }
