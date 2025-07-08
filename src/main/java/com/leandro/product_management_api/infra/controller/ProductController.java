7154c7b1567039ac139526c20035622481928676
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-08 20:28:52 -0300

    comments removed

--- src/main/java/com/leandro/product_management_api/infra/controller/ProductController.java
+++ src/main/java/com/leandro/product_management_api/infra/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.leandro.product_management_api.application.dtos.*;
 import com.leandro.product_management_api.application.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
