1d3ab44895f3111c043d142b98f748fe2e943c4f
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-08 20:59:53 -0300

    correcting annotation

--- src/main/java/com/leandro/product_management_api/infra/implementation/DetailsServiceImpl.java
+++ src/main/java/com/leandro/product_management_api/infra/implementation/DetailsServiceImpl.java
@@ -2,15 +2,15 @@ package com.leandro.product_management_api.infra.implementation;
 
 import com.leandro.product_management_api.core.domain.entity.User;
 import com.leandro.product_management_api.core.domain.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class DetailsServiceImpl implements UserDetailsService {
-    @Autowired
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
