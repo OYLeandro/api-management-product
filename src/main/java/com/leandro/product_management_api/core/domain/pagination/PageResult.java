aa1cf23ed9d41e84d03a4f2e8158f0962e44bc01
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-07-07 23:23:21 -0300

    correcting

--- src/main/java/com/leandro/product_management_api/core/domain/pagination/PageResult.java
+++ src/main/java/com/leandro/product_management_api/core/domain/pagination/PageResult.java
@@ -2,7 +2,7 @@ package com.leandro.product_management_api.core.domain.pagination;
 
 import java.util.List;
 
-public record Page<T>(
+public record PageResult<T>(
         List<T> items,
         int currentPage,
         int totalPage,
